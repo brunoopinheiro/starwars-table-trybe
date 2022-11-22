4ff835ed83bf3e0afb94f2d6128c6a7476bf47d4
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-11-22 14:44:56 -0300

    setup: starting project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Table />
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,17 @@
+export default function Table() {
+  return (
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Rotation Period</th>
+        <th>Orbital Period</th>
+        <th>Diameter</th>
+        <th>Climate</th>
+        <th>Gravity</th>
+        <th>Terrain</th>
+        <th>Surface Water</th>
+        <th>Population</th>
+      </tr>
+    </table>
+  );
+}
